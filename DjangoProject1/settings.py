75750efd7382eaa6414ae5e31bb250be63beb59a
admin_Orest <orest.yakobchuk@gmail.com>
2025-06-25 18:19:52 +0300

    Fix ROOT_URLCONF in settings.py

--- DjangoProject1/settings.py
+++ DjangoProject1/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'DjangoProject6.urls'
+ROOT_URLCONF = 'DjangoProject6git add ..urls'
 
 TEMPLATES = [
     {
